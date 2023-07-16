982407f87d650a8cb0729644ea9819594f134d15
JoãoSantos <jv29645@gmail.com>
2023-07-16 17:18:41 -0300

    showing data values

--- frontend/script/script.js
+++ frontend/script/script.js
@@ -1,20 +1,14 @@
 const key = 'e4c9c6f21512a6ac9c72c554798db96d'
-const apicountry = 'https://www.countryflagicons.com/FLAT/64/DE.png'
-
+var apicountry = 'https://www.countryflagicons.com/FLAT/64/'
 const cityInput = document.querySelector('#city_input')
 const searchButton = document.querySelector('#search')
 const cityElement = document.querySelector('#city')
 const temp = document.querySelector('#temperature span')
-const descripition = document.querySelector('#descripiton')
+const descripition = document.querySelector('#descripition')
 const weatherIcon = document.querySelector('#weather-icon')
 const country = document.querySelector('#country')
 const umidity = document.querySelector('#umdity span')
 const wind = document.querySelector('#wind span')
-
-
-const showWeathetData = (city)=>{
-    getWeatherData(city)
- }
 
 const getWeatherData = async(city)=>{
     const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}&lang=pt_br`
@@ -22,7 +16,19 @@ const getWeatherData = async(city)=>{
     const res = await fetch(apiWeatherURL)
     const data = await res.json()
     console.log(data)
+    return data
 }
+const showWeathetData = async (city)=>{
+    const data = await getWeatherData(city)
+
+    cityElement.innerText = data.name
+    temp.innerText = data.main.temp
+    descripition.innerText = data.weather[0].description
+    let countryCode = data.sys.country
+    apicountry =  `https://www.countryflagicons.com/FLAT/64/${countryCode}.png`
+    country.innerHTML = `src='${apicountry}'`
+ }
+
 
 searchButton.addEventListener('click',(e)=>{
     e.preventDefault()
